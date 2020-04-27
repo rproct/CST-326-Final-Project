fea4a7eb2660479ad3aca900a6ef7d2b6a42d5a1
Ryan Proctor <49620154+rproct@users.noreply.github.com>
2020-04-26 20:27:20 -0700

    Apr 26, 2020

---
+++ Rollout!/Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveVert = Input.GetAxis("Vertical");
+        
+        Vector3 move = new Vector3(0f, 0f, moveVert);
+
+        rb.AddForce(move * speed);
+    }
+}
---
+++ Rollout!/Assets/Scripts/RotateChair.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateChair : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(0.0f, Input.GetAxis("Horizontal") * speed, 0.0f);
+    }
+}
