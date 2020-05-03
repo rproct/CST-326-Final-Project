c89fe30326814c1a25fd1854762ccd79dd85b9de
GCanoSandoval <46907725+GCanoSandoval@users.noreply.github.com>
2020-05-03 14:32:42 -0700

    Add files via upload
    Tried to create a file that manipulated the gun movement to move up and down. Still incomplete. Used various forums to give me an idea of how to do such a thing.

---
+++ Rollout!/Assets/Scripts/GunMovement.cs
@@ -0,0 +1,55 @@
+﻿/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GunMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    public Vector3 targetPos;
+    public bool isMoving;
+    const int MOUSE = 0;
+
+    void Start()
+    {
+        targetPos = transform.position;
+        isMoving = false;
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButton(MOUSE))
+        {
+            SetTargetPosition();
+        }
+
+        if(isMoving)
+        {
+            MoveObject();
+        }
+    }
+
+    void SetTargetPosition()
+    {
+        Plane plane = new Plane(Vector3.up, transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float point = 0f;
+
+        if(plane.Raycast(ray, out point))
+            targetPos = ray.GetPoint(point);
+
+        isMoving = true;
+    }
+
+    void MoveObject()
+    {
+        transform.LookAt(targetPos);
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if(transform.position == targetPos)
+            isMoving = false;
+    }
+}
+
+    */
